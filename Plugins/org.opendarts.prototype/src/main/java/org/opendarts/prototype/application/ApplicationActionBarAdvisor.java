4d4e5eb3fbae0d5858f78a6c51d87165f6bc13c5
Igor Laborie <ilaborie@gmail.com>
2011-06-27 21:23:38 +0200

    Init: Create a minimal Eclipse RCP App

---
+++ Plugins/org.opendarts.prototype/src/main/java/org/opendarts/prototype/application/ApplicationActionBarAdvisor.java
@@ -0,0 +1,36 @@
+package org.opendarts.prototype.application;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+/**
+ * The Class ApplicationActionBarAdvisor.
+ */
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	/**
+	 * Instantiates a new application action bar advisor.
+	 *
+	 * @param configurer the configurer
+	 */
+	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+		super(configurer);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.application.ActionBarAdvisor#makeActions(org.eclipse.ui.IWorkbenchWindow)
+	 */
+	@Override
+	protected void makeActions(IWorkbenchWindow window) {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.application.ActionBarAdvisor#fillMenuBar(org.eclipse.jface.action.IMenuManager)
+	 */
+	@Override
+	protected void fillMenuBar(IMenuManager menuBar) {
+	}
+
+}
