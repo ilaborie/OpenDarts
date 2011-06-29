3ebe44375bf00e4cf18b3d41d57db91e5f151aaf
Igor Laborie <ilaborie@gmail.com>
2011-06-29 23:02:52 +0200

    UI: Add a basic menu with commands
    Commands added: Exit, About, new Game (no implementation)

---
+++ Plugins/org.opendarts.prototype/src/main/java/org/opendarts/prototype/ui/handler/NewGameHandler.java
@@ -0,0 +1,29 @@
+package org.opendarts.prototype.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Class NewGameHandler.
+ */
+public class NewGameHandler extends AbstractHandler implements IHandler {
+
+	/** The logger. */
+	private static final Logger LOG = LoggerFactory
+			.getLogger(NewGameHandler.class);
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		LOG.info("New Game...");
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
